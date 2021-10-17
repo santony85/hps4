a3e071a684eb58bbfda36e9fc5a78ca50c689859
Antony <antony_santero@hotmail.com>
2021-10-17 17:49:33 +0200

    start

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 ionic build --prod
-http-server ./www -p 8200 --ssl --cert /etc/letsencrypt/live/hps-crm.fr/fullchain.pem --key /etc/letsencrypt/live/hps-crm.fr/privkey.pem
+http-server ./www -p 8200 --ssl --cert /etc/letsencrypt/live/hps-crm.fr/fullchain.pem --key /etc/letsencrypt/live/hps-crm.fr/privkey.pem &&
