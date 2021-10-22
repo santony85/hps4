6905315519d4c985f48f8ae7512a7f102dc19775
Antony <antony_santero@hotmail.com>
2021-10-22 12:30:04 +0200

    ok xxx

--- src/app/formrapport/formrapport.module.ts
+++ src/app/formrapport/formrapport.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
-import { IonicSelectableModule } from 'ionic-selectable';
-import { FormrapportPageRoutingModule } from './formrapport-routing.module';
-import { FormrapportPage } from './formrapport.page';
+import { IonicModule } from "@ionic/angular";
+import { IonicSelectableModule } from "ionic-selectable";
+import { FormrapportPageRoutingModule } from "./formrapport-routing.module";
+import { FormrapportPage } from "./formrapport.page";
 //import { ModalproduitPageModule} from '../modalproduit/modalproduit.module'
 @NgModule({
   imports: [
@@ -15,8 +15,9 @@ import { FormrapportPage } from './formrapport.page';
     ReactiveFormsModule,
     IonicSelectableModule,
     //ModalproduitPageModule,
-    FormrapportPageRoutingModule
+    FormrapportPageRoutingModule,
   ],
-  declarations: [FormrapportPage]
+  declarations: [FormrapportPage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class FormrapportPageModule {}
